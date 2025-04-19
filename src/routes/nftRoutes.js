5f744afb9845d35d04d6bd79e92d138629427a28
kupets <kupets@evergreens.in.ua>
2025-04-19 20:33:24 +0300

    feat: add new nft route

--- src/routes/nftRoutes.js
+++ src/routes/nftRoutes.js
@@ -3,5 +3,6 @@ const nftController = require('../controllers/nftController');
 const router = express.Router();
 
 router.post('/v1/nft/certificates', nftController.mint);
+router.post('/v1/nft/draft', nftController.draft);
 
 module.exports = router;
