931eaa44f61e76405220f58ed6b38748d48ca112
kupets <kupets@evergreens.in.ua>
2025-06-12 08:55:31 +0300

    chore: delete comments

--- src/routes/documentRoutes.js
+++ src/routes/documentRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController');
 
-// POST /api/documents - створити документ
 router.post('/', documentController.createDocument);
 
-// GET /api/documents?page=1&limit=10&type=passport&userId=123
 router.get('/', documentController.getDocuments);
 
 module.exports = router;
