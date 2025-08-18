ea1c36c7715a7633c024b3eb9bc6f3eb8d1b0bbb
kupets <kupets@evergreens.in.ua>
2025-08-18 23:11:58 +0300

    chore: add inteceprto for file

--- src/protect/protect.controller.ts
+++ src/protect/protect.controller.ts
@@ -2,13 +2,27 @@ import { Controller, Post, UseInterceptors, UploadedFile, Body } from '@nestjs/c
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateCertificateDto } from './dto/create-certificate.dto/create-certificate.dto';
 import { ProtectService } from './protect.service';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { extname } from 'path';
 
 @Controller('protect')
 export class ProtectController {
   constructor(private readonly protectService: ProtectService) {}
 
   @Post('/issue')
-  @UseInterceptors(FileInterceptor('attachments'))
+  @UseInterceptors(
+    FileInterceptor('attachments', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = uuidv4();
+          const ext = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
+      }),
+    }),
+  )
   async issueCertificate(
     @UploadedFile() file: Express.Multer.File,
     @Body() createCertificateDto: CreateCertificateDto,
