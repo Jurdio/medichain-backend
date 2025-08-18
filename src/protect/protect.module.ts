370b44a468543d19f2cb2865bfce32c478b7be1e
kupets <kupets@evergreens.in.ua>
2025-08-18 19:44:46 +0300

    fix: fix build

--- src/protect/protect.module.ts
+++ src/protect/protect.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ProtectController } from './protect.controller';
 import { ProtectService } from './protect.service';
-import { ProtectController } from './protect.controller';
 
 @Module({
   controllers: [ProtectController],
